00bd932849e114f7bdbb8d5fef72da5a12f0abee
Anna Popova <panasenko-anna.8@mail.ru>
2022-08-20 18:46:12 +0300

    add decision

--- src/getUserCharacteristic.js
+++ src/getUserCharacteristic.js
@@ -6,53 +6,33 @@ const character = {
 	attack: 40,
 	defence: 10,
 	special: [
-	  {
-		 id: 8,
-		 name: 'Двойной выстрел',
-		 icon: 'http://...',
-		 description: 'Двойной выстрел наносит двойной урон'
-	  },
-	  {
-		 id: 9,
-		 name: 'Нокаутирующий удар',
-		 icon: 'http://...'
-		 // <- обратите внимание, описание "засекречено"
-	  }
-	]
- }
- 
- function getUserСharacteristic(objProperty) {
-	const resultArr = [];
- 
-	const specialArr = objProperty;
-  // console.log(specialArr);
- 
-	specialArr.forEach((item) => {
-    // console.log(item);
-	  // resultArr.push(item);
+		{
+		id: 8,
+		name: 'Двойной выстрел',
+		icon: 'http://...',
+		description: 'Двойной выстрел наносит двойной урон',
+		},
+		{
+		id: 9,
+		name: 'Нокаутирующий удар',
+		icon: 'http://...',
+		// <- обратите внимание, описание "засекречено"
+		},
+	],
+};
 
-    //здесь я деструктурирую объекты из массива и назначаю дефолтное значение для description
-	  const {id, name, icon, description = 'Описание недоступно'} = item;
-    //? вопрос, как теперь эти переменные собрать обратно в массив объектов
-	  console.log(id, name, icon, description);
- 
+function getUserСharacteristic({
+ id, name, icon, description = 'Описание недоступно',
+}) {
+	const resultArr = [];
+
+	character.special.forEach((item) => {
+		item.description = item.description ? item.description : 'Описание недоступно';
+
+		resultArr.push(item);
 	});
- 
-	console.log(resultArr);
- 
- 
+
 	return resultArr;
- }
- 
- getUserСharacteristic(character.special);
- 
- // Напишите функцию с аргументом-деструктором, которая извлекает массив с нужными полями (id, name, description, icon) из объекта, а если значения для поля description нет - устанавливает default'ное значение в 'Описание недоступно'. Функция должна возвращать извлечённый массив из объектов с четыремя полями.
- 
- // [
- //   {
- //     id: 9,
- //     name: 'Нокаутирующий удар',
- //     icon: 'http://...',
- //     description: 'Описание недоступно'
- //   }
- // ]
+}
+
+console.log(getUserСharacteristic(character.special));
