62c3536c280f73a9d5daa011430acf8b9b9ddb57
Anna Popova <panasenko-anna.8@mail.ru>
2022-08-20 19:01:59 +0300

    fixdecision

---
+++ src/__tests__/getUserСharacteristic.js
@@ -0,0 +1,40 @@
+import getUserСharacteristic from '../getUserCharacteristic';
+
+test('testing getUserСharacteristic function', () => {
+	const character = {
+		name: 'Лучник',
+		type: 'Bowman',
+		health: 50,
+		level: 3,
+		attack: 40,
+		defence: 10,
+		special: [
+			{
+			id: 8,
+			name: 'Двойной выстрел',
+			icon: 'http://...',
+			description: 'Двойной выстрел наносит двойной урон',
+			},
+			{
+			id: 9,
+			name: 'Нокаутирующий удар',
+			icon: 'http://...',
+			},
+		],
+	};
+	const result = getUserСharacteristic(character.special);
+	expect(result).toEqual([
+		{
+			id: 8,
+			name: 'Двойной выстрел',
+			icon: 'http://...',
+			description: 'Двойной выстрел наносит двойной урон',
+		},
+		{
+			id: 9,
+			name: 'Нокаутирующий удар',
+			icon: 'http://...',
+			description: 'Описание недоступно',
+		},
+	]);
+});
--- src/getUserCharacteristic.js
+++ src/getUserCharacteristic.js
@@ -1,32 +1,7 @@
-const character = {
-	name: 'Лучник',
-	type: 'Bowman',
-	health: 50,
-	level: 3,
-	attack: 40,
-	defence: 10,
-	special: [
-		{
-		id: 8,
-		name: 'Двойной выстрел',
-		icon: 'http://...',
-		description: 'Двойной выстрел наносит двойной урон',
-		},
-		{
-		id: 9,
-		name: 'Нокаутирующий удар',
-		icon: 'http://...',
-		// <- обратите внимание, описание "засекречено"
-		},
-	],
-};
-
-function getUserСharacteristic({
- id, name, icon, description = 'Описание недоступно',
-}) {
+export default function getUserСharacteristic(objProperty) {
 	const resultArr = [];
 
-	character.special.forEach((item) => {
+	objProperty.forEach((item) => {
 		item.description = item.description ? item.description : 'Описание недоступно';
 
 		resultArr.push(item);
@@ -34,5 +9,3 @@ function getUserСharacteristic({
 
 	return resultArr;
 }
-
-console.log(getUserСharacteristic(character.special));
