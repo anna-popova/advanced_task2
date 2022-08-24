a27ccd56e9f92579e8e649b015378aca4c055629
Anna Popova <61383401+anna-popova@users.noreply.github.com>
2022-08-24 10:55:12 +0300

    Update getUserCharacteristic.js

--- src/getUserCharacteristic.js
+++ src/getUserCharacteristic.js
@@ -1,11 +1,31 @@
-export default function getUserСharacteristic(objProperty) {
-	const resultArr = [];
+// export default function getUserСharacteristic(objProperty) {
+// 	const resultArr = [];
 
-	objProperty.forEach((item) => {
-		item.description = item.description ? item.description : 'Описание недоступно';
+// 	objProperty.forEach((item) => {
+// 		item.description = item.description ? item.description : 'Описание недоступно';
 
-		resultArr.push(item);
-	});
+// 		resultArr.push(item);
+// 	});
 
-	return resultArr;
+// 	return resultArr;
+// }
+
+export default function getUserСharacteristic(obj) {
+  const resultArr = [];
+
+  obj.special.forEach((item) => {
+    const {id, name, icon, description = 'Описание недоступно'} = item;
+    // console.log(id, name, icon, description);
+    
+    const obj = {};
+    obj.id = id;
+    obj.name = name;
+    obj.icon = icon;
+    obj.description = description;
+    // console.log(obj);
+    
+    resultArr.push(obj);
+  });
+  
+  return resultArr;
 }
