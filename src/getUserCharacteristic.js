e2bb3fa39f70374b91633499a8428589c25263ea
Anna Popova <panasenko-anna.8@mail.ru>
2022-08-28 15:34:56 +0300

    add another decision

--- src/getUserCharacteristic.js
+++ src/getUserCharacteristic.js
@@ -10,24 +10,28 @@
 // 	return resultArr;
 // }
 
-export default function getUserСharacteristic(object) {
-  const resultArr = [];
+// export default function getUserСharacteristic(object) {
+//   const resultArr = [];
 
-  object.special.forEach((item) => {
-    const {
- id, name, icon, description = 'Описание недоступно',
-} = item;
-    // console.log(id, name, icon, description);
+//   object.special.forEach((item) => {
+//     const {
+//  id, name, icon, description = 'Описание недоступно',
+// } = item;
+//     // console.log(id, name, icon, description);
 
-    const obj = {};
-    obj.id = id;
-    obj.name = name;
-    obj.icon = icon;
-    obj.description = description;
-    // console.log(obj);
+//     const obj = {};
+//     obj.id = id;
+//     obj.name = name;
+//     obj.icon = icon;
+//     obj.description = description;
+//     // console.log(obj);
 
-    resultArr.push(obj);
-  });
+//     resultArr.push(obj);
+//   });
 
-  return resultArr;
+//   return resultArr;
+// }
+
+export default function getUserСharacteristic({ special = [] }) {
+	return special.map(({ description = 'Описание недоступно', ...item }) => ({ ...item, description }));
 }
